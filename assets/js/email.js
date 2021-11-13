f2018419b1c665c5d7103b7d291d4058866b4b7e
Ifti Khan <ifti_k@hotmail.com>
2021-11-13 18:15:02 +0000

    Mod: Contact form js logic added

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,2 +1,40 @@
-const btn = document.getElementById('sendbutton');
+const btn = document.getElementById('sendBtn');
 
+function changeSendBtnText() {
+    btn.textContent = 'Send';
+}
+
+function resetContactForm() {
+    document.getElementById('contactForm').reset();
+}
+
+document.getElementById('contactForm')
+    .addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        btn.textContent = 'Sending...';
+
+        const serviceID = 'aph-gmail-service';
+        const templateID = 'my-default-template';
+
+        emailjs.send(serviceID, templateID, {
+                "from_name": this.name.value,
+                "message": this.message.value,
+                "from_email": this.email.value,
+                "contact_number": this.number.value,
+            })
+            .then((response) => {
+                btn.textContent = 'Sent';
+                console.log("Message Has Been Sent", response);
+                resetContactForm();
+                setTimeout(changeSendBtnText, 5000);
+
+            }, (error) => {
+                btn.textContent = 'Error';
+                console.log("Message Failed To Send", error);
+                alert(`Oops something went wrong!`);
+                resetContactForm();
+                setTimeout(changeSendBtnText, 3000);
+                //window.location.replace("assets/pages/message_404.html");
+            });
+    });
