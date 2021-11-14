f93eaf82a024c22c402a335d2a4af97828a58ae7
Ifti Khan <ifti_k@hotmail.com>
2021-11-14 19:53:35 +0000

    Mod: Back to top btn added and header re-styled

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,24 @@
 $(document).ready(function () {
-    // This is for the copyright year located in the footer 
+    //This is for the copyright year located in the footer 
     //and will auto update when the year date changes
     $("#copyright-footerYear").text(new Date().getFullYear());
+
+    //This block of code is for the back to top button
+    //This part of the code makes the button visible when the user scrolls
+    let topButton = $('#back-to-top-btn');
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 50) {
+            topButton.addClass('visible');
+        } else {
+            topButton.removeClass('visible');
+        }
+    });
+
+    //This is part of the floating scroll button, so when a user clicks the button 
+    //it will take the user back to the top of the page.
+    topButton.on('click', function (event) {
+        event.preventDefault();
+        $('html, body').scrollTop(0);
+    });
 
 });
